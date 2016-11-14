a9110f5a544f673e9174f65a5366655a292ade92
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-11-13 16:26:18 -0800

    Use AzureSync multiplexer

--- opwen_email_server/ioc.py
+++ opwen_email_server/ioc.py
@@ -4,7 +4,7 @@ from opwen_domain.email.tinydb import TinyDbEmailStore
 from opwen_domain.mailbox.sendgrid import SendGridEmailReceiver
 from opwen_domain.mailbox.sendgrid import SendGridEmailSender
 from opwen_domain.sync.azure import AzureAuth
-from opwen_domain.sync.azure import AzureSync
+from opwen_domain.sync.azure import MultiClientAzureSync
 from opwen_infrastructure.serialization.json import JsonSerializer
 
 from opwen_email_server.config import FlaskConfig
@@ -22,14 +22,20 @@ class Ioc(object):
     email_sender = SendGridEmailSender(
         apikey=FlaskConfig.SENDGRID_ACCOUNT_KEY)
 
-    email_sync = AzureSync(
+    email_sync = MultiClientAzureSync(
         auth=AzureAuth(
             account=OpwenConfig.STORAGE_ACCOUNT_NAME,
             key=OpwenConfig.STORAGE_ACCOUNT_KEY,
             container=OpwenConfig.STORAGE_CONTAINER),
-        download_locations=[OpwenConfig.STORAGE_UPLOAD_PATH],
-        upload_locations=[OpwenConfig.STORAGE_UPLOAD_PATH],
-        serializer=JsonSerializer())  # TODO: create multiplexer
+        email_info=(
+            # the two lines below are not a bug: we want to inverse the up/download
+            # because all configs are named from the point of view of the clients
+            # e.g. if a client uploads to ~foo and downloads from ~bar then the
+            # server needs to upload to ~bar and download from ~foo
+            OpwenConfig.STORAGE_UPLOAD_FORMAT,
+            OpwenConfig.STORAGE_DOWNLOAD_FORMAT,
+            OpwenConfig.EMAIL_HOST_FORMAT.format('')),
+        serializer=JsonSerializer())
 
 
 def create_app():
