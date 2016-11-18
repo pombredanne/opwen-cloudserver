0009e275be3025885a45aa667e569a207fb1628a
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-11-18 11:24:42 -0800

    Split out cron jobs into own module

--- opwen_email_server/__init__.py
+++ opwen_email_server/__init__.py
@@ -3,3 +3,11 @@ from opwen_email_server.ioc import create_app
 app = create_app()
 
 from opwen_email_server import views
+
+from opwen_email_server.crons import setup_client_email_download_cron
+from opwen_email_server.crons import setup_client_email_send_cron
+from opwen_email_server.crons import setup_client_email_upload_cron
+
+setup_client_email_download_cron()
+setup_client_email_upload_cron()
+setup_client_email_send_cron()
--- opwen_email_server/views.py
+++ opwen_email_server/views.py
@@ -1,14 +1,12 @@
 from flask import request
-from opwen_infrastructure.cron import setup_cronjob
-from opwen_infrastructure.logging import log_execution
 from opwen_infrastructure.flask import debug_route
+from opwen_infrastructure.logging import log_execution
 
 from opwen_email_server import app
-from opwen_email_server.actions import DownloadEmailsFromClients
 from opwen_email_server.actions import ReceiveEmail
-from opwen_email_server.actions import SendEmailsFromClients
-from opwen_email_server.actions import UploadEmailsToClients
-from opwen_email_server.config import AppConfig
+from opwen_email_server.crons import client_email_download
+from opwen_email_server.crons import client_email_send
+from opwen_email_server.crons import client_email_upload
 
 
 @app.route('/inbox', methods=['POST'])
@@ -22,68 +20,23 @@ def inbox():
     return 'OK'
 
 
-@app.before_first_request
-@log_execution(app.logger)
-def _setup_upload_emails_to_clients_cron():
-    upload_hour = str(AppConfig.UPLOAD_EMAILS_TO_CLIENT_HOUR_UTC)
-    setup_cronjob(hour_utc=upload_hour,
-                  method=_upload_emails_to_clients,
-                  logger=app.logger,
-                  description='Upload server emails to client at {} UTC'.format(upload_hour))
-
-
-@app.before_first_request
-@log_execution(app.logger)
-def _setup_download_emails_from_clients_cron():
-    download_hour = str(AppConfig.DOWNLOAD_CLIENT_EMAILS_HOUR_UTC)
-    setup_cronjob(hour_utc=download_hour,
-                  method=_download_emails_from_clients,
-                  logger=app.logger,
-                  description='Download client emails to server at {} UTC'.format(download_hour))
-
-
-@app.before_first_request
-@log_execution(app.logger)
-def _setup_send_emails_from_clients_cron():
-    send_hour = str(AppConfig.SEND_CLIENT_EMAILS_HOUR_UTC)
-    setup_cronjob(hour_utc=send_hour,
-                  method=_send_email_from_clients,
-                  logger=app.logger,
-                  description='Send client emails via server at {} UTC'.format(send_hour))
-
-
-@debug_route(app, '/upload')
-@log_execution(app.logger)
-def _upload_emails_to_clients():
-    upload_emails_to_clients = UploadEmailsToClients(
-        email_store=app.ioc.received_email_store,
-        email_sync=app.ioc.email_sync)
-
-    upload_emails_to_clients()
+@debug_route(app, '/download')
+def download():
+    client_email_download()
 
     return 'OK'
 
 
 @debug_route(app, '/send')
-@log_execution(app.logger)
-def _send_email_from_clients():
-    send_emails_from_clients = SendEmailsFromClients(
-        email_sender=app.ioc.email_sender,
-        email_store=app.ioc.client_email_store)
-
-    send_emails_from_clients()
+def send():
+    client_email_send()
 
     return 'OK'
 
 
-@debug_route(app, '/download')
-@log_execution(app.logger)
-def _download_emails_from_clients():
-    download_emails_from_clients = DownloadEmailsFromClients(
-        email_sync=app.ioc.email_sync,
-        email_store=app.ioc.client_email_store)
-
-    download_emails_from_clients()
+@debug_route(app, '/upload')
+def upload():
+    client_email_upload()
 
     return 'OK'
 
